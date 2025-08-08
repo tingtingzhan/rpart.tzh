4ba68b21af39ab676e273d7c0d956f5bcd615f1b
Tingting Zhan <tingtingzhan@gmail.com>
2025-08-07 20:11:55 -0400

    tzh

--- R/md_.rpart.R
+++ R/md_.rpart.R
@@ -13,20 +13,21 @@
 #' Function [md_.rpart()] returns a \link[base]{character} \link[base]{vector}.
 #' 
 #' @examples
-#' library(rmd.tzh); list(
+#' list(
 #'  '`rpart`' = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis, model = TRUE)
-#' ) |> render_(file = 'rpart')
+#' ) |> rmd.tzh::render_(file = 'rpart')
 #' 
 #' @keywords internal
 #' @importFrom rmd.tzh md_ md_.default
 #' @importClassesFrom rmd.tzh md_lines
 #' @importFrom methods new
+#' @importFrom utils bibentry
 #' @export md_.rpart
 #' @export
 md_.rpart <- function(x, ...) {
   
   attr(x, which = 'text') <- sprintf(
-    fmt = 'Recursive partitioning and regression tree for **`%s`** based on potential covariate(s) %s is provided by <u>**`R`**</u> package <u>**`rpart`**</u>. The data-driven partition, i.e., the collection of cutoff value(s), is based on %d observations.', 
+    fmt = 'Recursive partitioning and regression tree [@Breiman84] for **`%s`** based on potential covariate(s) %s is provided by <u>**`R`**</u> package <u>**`rpart`**</u>. The data-driven partition, i.e., the collection of cutoff value(s), is based on %d observations.', 
     x$terms[[2L]] |> deparse1(),
     x$terms[[3L]] |> 
       all.vars() |>
@@ -34,7 +35,16 @@ md_.rpart <- function(x, ...) {
       paste(collapse = ', '),
     x$frame$n[1L]
   ) |> 
-    new(Class = 'md_lines', package = 'rpart')
+    new(Class = 'md_lines', package = 'rpart', bibentry = bibentry(
+      bibtype = 'Book', key = 'Breiman84',
+      author = c('Leo Breiman', 'Jerome Friedman', 'Richard A. Olshen', 'Charles J. Stone'),
+      title = 'Classification and Regression Trees',
+      year = '1984',
+      edition = '1',
+      publisher = 'Chapman and Hall/CRC',
+      address = 'New York',
+      doi = '10.1201/9781315139470'
+    ))
   
   md_.default(x, ...)
   
