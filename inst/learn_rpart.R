fd390d961527c88b457310748fc6238d0ae0fdbd
Tingting Zhan <tingtingzhan@gmail.com>
2026-05-01 16:49:44 -0400

    tzh

--- inst/learn_rpart.R
+++ inst/learn_rpart.R
@@ -1,9 +1,9 @@
 
 # ?rpart::rpart
 library(rpart)
-set.seed(1314); m1 = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
-set.seed(1314); m1a = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
-set.seed(125); m2 = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+set.seed(14); m1 = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+set.seed(14); m1a = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+set.seed(25); m2 = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
 
 stopifnot(
   identical(m1, m1a),
